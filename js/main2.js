b58f4eecaebeac7a21cf7e92c025fd11941ae35d
btcorso <uniebcorso@fei.edu.br>
2022-10-17 17:21:59 -0300

    finalizando o codigo

--- js/main2.js
+++ js/main2.js
@@ -21,13 +21,14 @@ form.addEventListener("submit", (event) => {
 
     if (existe) {
         console.log("Item repetido");
-        itemAtual.id = existe.id; //o id do objeto itemAtual recebe o id do obejto 'existe'
+        itens[itens.findIndex(x => x.id === existe.id)] = itemAtual; //busca o item atual dentro do array existente
+        itemAtual.id = existe.id; // garante que irá atribuir o mesmo id do atual para o item
         atualizaQtd(itemAtual); // chamar a função para atualizar a quantidade
         itens[existe.id] = itemAtual; //atualiza o objeto no array 'itens'
     }
     else{
         console.log("Item novo!");
-        itemAtual.id = itens.length; //estabelece que o id será do valor do lenght do array 'itens' naquele momento
+        itemAtual.id = itens[itens.length-1] ? (itens[itens.length-1]).id +1 : 0; //estabelece que o id será do valor do lenght do array 'itens' naquele momento
         criaElemento(itemAtual); //chamar a função para adicionar item na lista
         itens.push(itemAtual); // adicionado o objeto no array []
     }
@@ -44,7 +45,7 @@ function criaElemento(item) {
     qtdItem.dataset.id = item.id; // atribuiu o data-id dentro do elemento
     novoItem.appendChild(qtdItem); //inputando o elemento 'qtdItem' para dntro do elemento 'novoItem'
     novoItem.innerHTML += item.nome; // adicionando dentro do elemento 'novoItem' o 'nome' objeto enviado como parâmetro
-    novoItem.appendChild(botaoDeleta()); //inputa o elemento botão dentro do elemento 'novoItem' 
+    novoItem.appendChild(botaoDeleta(item.id)); //inputa o elemento botão dentro do elemento 'novoItem' 
     lista.appendChild(novoItem); //inputando o elemento 'novoItem' para dentro da lista
 }
 
@@ -52,17 +53,17 @@ function atualizaQtd(item){
     document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade; // busca pelo data- e atualiza o valor da quantidade do objeto
 }
 
-function botaoDeleta(){
+function botaoDeleta(id){
     const elementoBotao = document.createElement('button'); //criando o elemento tag <button>
     elementoBotao.innerHTML = "X"; //escrevendo 'x' no botão
-
     elementoBotao.addEventListener("click", function() {
-        deletaElemento(this.parentNode); //chama a função para deletar o elemento pai do botão
+        deletaElemento(this.parentNode, id); //chama a função para deletar o elemento pai do botão
     })
-
     return elementoBotao; //retorna valor para a função appendChild ter algo para inputar
 }
 
-function deletaElemento(element){
-    element.remove();
+function deletaElemento(element, id){
+    element.remove(); //remove o elemento html
+    itens.splice(itens.findIndex(x => x.id === id), 1); //remove o objeto do array 
+    localStorage.setItem("itens", JSON.stringify(itens)); //atualiza o objeto dentro do localStorage dentro da Key 'itens'
 }
