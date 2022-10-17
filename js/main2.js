514a855c4d374bcaac0b6fdd37042e7a13cd4d1d
btcorso <uniebcorso@fei.edu.br>
2022-10-17 00:13:47 -0300

    Js para incluir novo item

--- js/main2.js
+++ js/main2.js
@@ -1,9 +1,22 @@
 const form = document.getElementById("novoItem");
+const lista = document.getElementById("lista");
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    console.log(event);
-    console.log(event.target.elements["nome"])
-    console.log(event.target.elements["quantidade"])
-
+    criaElemento(event.target.elements["nome"].value, event.target.elements["quantidade"].value)
 })
+
+function criaElemento(nome, qtd) {
+    console.log(nome);
+    console.log(qtd);
+
+    const novoItem = document.createElement('li');
+    novoItem.classList.add("item");
+    const qtdItem = document.createElement('strong');
+    qtdItem.innerHTML = qtd;
+    novoItem.appendChild(qtdItem);
+    novoItem.innerHTML += nome;
+    lista.appendChild(novoItem);
+    console.log(qtdItem);
+    console.log(novoItem);
+}
